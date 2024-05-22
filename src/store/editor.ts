182ae2764a2cb0003979c5d8328697ca7e4c6a0f
Ulrich <ulrichtonmoy1@gmail.com>
2024-05-23 05:00:39 +0600

    update selectedToolAtom types

--- src/store/editor.ts
+++ src/store/editor.ts
@@ -1,7 +1,7 @@
 import { ToolType } from "@/libs";
 import { atom } from "jotai";
 
-export const selectedToolAtom = atom<ToolType>(ToolType.NULL);
+export const selectedToolAtom = atom<ToolType | null>(null);
 
 export const updateSelectedToolAtom = atom(null, async (_get, set, type: ToolType) => {
   set(selectedToolAtom, type);
