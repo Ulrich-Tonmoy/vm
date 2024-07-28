d2a003293f99521bbb1f9222dcb21ff8e10c4da2
Tonmoy <ulrichtonmoy1@gmail.com>
2024-07-28 23:25:55 +0600

    style: installed and available list style updated

--- src/components/body/Available.tsx
+++ src/components/body/Available.tsx
@@ -6,6 +6,7 @@ import {
 import { useAtomValue, useSetAtom } from "jotai";
 import { useEffect, useState } from "react";
 import { NodeVersionListModel } from "@/libs";
+import { MdInstallDesktop } from "react-icons/md";
 
 export const Available = () => {
   const loadNodeVersion = useSetAtom(loadNodeVersionAtom);
@@ -31,49 +32,75 @@ export const Available = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-5">
       <title>{selectedTool}</title>
-      <div className="mb-4 text-xl font-black">Available For Installation</div>
+      <div className="p-1 px-2 mb-4 text-xl font-black rounded-md shadow-xl md:text-4xl backdrop-blur-sm bg-slate-400/30">
+        Available For Installation
+      </div>
       <div className="flex justify-between gap-4">
         <div className="flex flex-col items-center space-y-2">
-          <div className="font-black">Current</div>
+          <div className="p-1 px-2 text-sm font-black rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30">
+            Current
+          </div>
           {currentVersions.map((v, i) => (
-            <div key={i}>
+            <div
+              key={i}
+              className="flex flex-wrap items-center justify-center p-1 px-2 text-sm rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30"
+            >
               {v.version}
-              <button className="p-1 ml-1 rounded-md hover:bg-slate-600 bg-slate-900">
-                Install
-              </button>
+              <MdInstallDesktop
+                className="p-1 ml-1 rounded-md hover:bg-slate-600 bg-slate-900 size-8"
+                title="Install"
+              />
             </div>
           ))}
         </div>
         <div className="flex flex-col items-center space-y-2">
-          <div className="font-black">LTS</div>
+          <div className="p-1 px-2 text-sm font-black rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-green-500/70 ">
+            LTS
+          </div>
           {ltsVersions.map((v, i) => (
-            <div key={i}>
+            <div
+              key={i}
+              className="flex flex-wrap items-center justify-center p-1 px-2 text-sm rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30"
+            >
               {v.version}
-              <button className="p-1 ml-1 rounded-md hover:bg-slate-600 bg-slate-900">
-                Install
-              </button>
+              <MdInstallDesktop
+                className="p-1 ml-1 rounded-md hover:bg-slate-600 bg-slate-900 size-8"
+                title="Install"
+              />
             </div>
           ))}
         </div>
         <div className="flex flex-col items-center space-y-2">
-          <div className="font-black">Old Stable</div>
+          <div className="p-1 px-2 text-sm font-black rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30">
+            Old Stable
+          </div>
           {stableVersions.map((v, i) => (
-            <div key={i}>
+            <div
+              key={i}
+              className="flex flex-wrap items-center justify-center p-1 px-2 text-sm rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30"
+            >
               {v.version}
-              <button className="p-1 ml-1 rounded-md hover:bg-slate-600 bg-slate-900">
-                Install
-              </button>
+              <MdInstallDesktop
+                className="p-1 ml-1 rounded-md hover:bg-slate-600 bg-slate-900 size-8"
+                title="Install"
+              />
             </div>
           ))}
         </div>
         <div className="flex flex-col items-center space-y-2">
-          <div className="font-black">Old UnStable</div>
+          <div className="p-1 px-2 text-sm font-black rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30">
+            Old UnStable
+          </div>
           {unStableVersions.map((v, i) => (
-            <div key={i}>
+            <div
+              key={i}
+              className="flex flex-wrap items-center justify-center p-1 px-2 text-sm rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30"
+            >
               {v.version}
-              <button className="p-1 ml-1 rounded-md hover:bg-slate-600 bg-slate-900">
-                Install
-              </button>
+              <MdInstallDesktop
+                className="p-1 ml-1 rounded-md hover:bg-slate-600 bg-slate-900 size-8"
+                title="Install"
+              />
             </div>
           ))}
         </div>
--- src/components/body/Installed.tsx
+++ src/components/body/Installed.tsx
@@ -1,3 +1,6 @@
+import { ImCheckboxChecked } from "react-icons/im";
+import { RiUninstallFill } from "react-icons/ri";
+
 interface InstalledProps {
   active: string;
   installed: string[];
@@ -6,21 +9,28 @@ interface InstalledProps {
 export const Installed = ({ active, installed }: InstalledProps) => {
   return (
     <div className="flex flex-col items-center p-4">
-      <div className="mb-4 text-xl font-black">Installed</div>
-      <div className="flex flex-wrap items-center justify-center gap-2 ml-8 space-y-2 md:m-0">
+      <div className="p-1 px-2 mb-4 text-xl font-black rounded-md shadow-xl md:text-4xl backdrop-blur-sm bg-slate-400/30">
+        Installed
+      </div>
+      <div className="flex flex-wrap items-center justify-center gap-2 ml-8 md:m-0">
         {installed.map((i) => (
-          <div key={i}>
+          <div
+            key={i}
+            className="flex items-center justify-center p-1 px-2 text-sm rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30"
+          >
             {i}
             {active === i ? (
-              <span className="ml-2 text-green-400">(Active)</span>
+              <ImCheckboxChecked className="ml-2 text-green-400 size-6" title="Active" />
             ) : (
               <>
-                <button className="p-1 ml-2 rounded-md hover:bg-slate-600 bg-slate-900">
-                  Use
-                </button>
-                <button className="p-1 ml-2 rounded-md hover:bg-rose-900 bg-rose-500">
-                  Uninstall
-                </button>
+                <ImCheckboxChecked
+                  className="ml-2 cursor-pointer bg-slate-500 hover:text-slate-600 text-slate-900 size-6"
+                  title="Use"
+                />
+                <RiUninstallFill
+                  className="ml-2 cursor-pointer hover:text-rose-900 text-rose-500 size-6"
+                  title="Uninstall"
+                />
               </>
             )}
           </div>
