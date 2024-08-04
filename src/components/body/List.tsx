5ae5bc8f86a44bc98f66aca3bbe55029b9315e63
Tonmoy <ulrichtonmoy1@gmail.com>
2024-08-04 23:19:13 +0600

    ✨feat: dont show expand it the items < 20

--- src/components/body/List.tsx
+++ src/components/body/List.tsx
@@ -10,41 +10,42 @@ interface ListProps {
 }
 
 export const List = ({ name, list }: ListProps) => {
-  const searchTerm = useAtomValue(searchTermAtom);
   const [limit, setLimit] = useState(LIST_LIMIT);
+  const searchTerm = useAtomValue(searchTermAtom);
+  const newList = list.filter((v) => v.version.includes(searchTerm));
+
+  const expand = newList.length > limit;
+  const collapse = newList.length === limit;
 
   return (
     <div className="flex flex-col items-center space-y-2">
       <div className="p-1 px-2 text-sm font-black rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30">
         {name}
       </div>
-      {list
-        .filter((v) => v.version.includes(searchTerm))
-        .slice(0, limit)
-        .map((v, i) => (
-          <div
-            key={i}
-            className="flex flex-wrap items-center justify-center p-1 px-2 text-sm rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30"
+      {newList.slice(0, limit).map((v, i) => (
+        <div
+          key={i}
+          className="flex flex-wrap items-center justify-center p-1 px-2 text-sm rounded-md shadow-xl lg:text-2xl backdrop-blur-sm bg-slate-400/30"
+        >
+          {v.version}
+          <button
+            className="p-1 ml-1 rounded-md hover:bg-slate-600 bg-slate-900 size-8"
+            title="Install"
           >
-            {v.version}
-            <button
-              className="p-1 ml-1 rounded-md hover:bg-slate-600 bg-slate-900 size-8"
-              title="Install"
-            >
-              <MdInstallDesktop />
-            </button>
-          </div>
-        ))}
-      {limit !== list.length && (
+            <MdInstallDesktop />
+          </button>
+        </div>
+      ))}
+      {expand && (
         <button
           className="flex items-center justify-center rounded-md hover:bg-slate-600 bg-slate-900"
           title="Show More"
-          onClick={() => setLimit(list.length)}
+          onClick={() => setLimit(newList.length)}
         >
           <MdOutlineExpandMore className="size-8" />
         </button>
       )}
-      {limit === list.length && (
+      {collapse && (
         <button
           className="flex items-center justify-center rounded-md hover:bg-slate-600 bg-slate-900"
           title="Collapse"
