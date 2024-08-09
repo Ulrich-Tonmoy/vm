47751f108fa0051a6eabec42abf36eb8dc7fd571
Tonmoy <ulrichtonmoy1@gmail.com>
2024-08-10 03:55:54 +0600

    🎨style: body layout updated for empty

--- src/components/ContentLayout.tsx
+++ src/components/ContentLayout.tsx
@@ -5,10 +5,7 @@ export const ContentLayout = forwardRef<HTMLDivElement, ComponentProps<"div">>(
   ({ children, className, ...props }, ref) => (
     <div
       ref={ref}
-      className={twMerge(
-        "flex flex-row h-[calc(100vh-28px)] mt-7 flex-1 overflow-auto",
-        className,
-      )}
+      className={twMerge("flex flex-row h-[calc(100vh-28px)] mt-7", className)}
       {...props}
     >
       {children}
--- src/components/body/ContentBody.tsx
+++ src/components/body/ContentBody.tsx
@@ -25,33 +25,35 @@ export const ContentBody = () => {
   };
 
   return (
-    <div className="flex flex-col pb-10 mt-1 mb-16 space-y-4">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <div className="flex">
-          <span className="p-1 px-3 m-1 mx-3 text-xl font-black rounded-md text-md lg:text-4xl backdrop-blur-sm bg-slate-400/30">
-            {selectedTool} Version Manager
-          </span>
-          <Button
-            variant="outline"
-            size="icon"
-            content={<ExitIcon className="size-7" />}
-            tooltip="Close"
-            onClick={() => updateSelectedTool(null)}
-          />
+    <div className="flex-1 mt-6 overflow-auto">
+      <div className="flex flex-col pb-10 mt-1 mb-16 space-y-4">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="flex">
+            <span className="p-1 px-3 m-1 mx-3 text-xl font-black rounded-md text-md lg:text-4xl backdrop-blur-sm bg-slate-400/30">
+              {selectedTool} Version Manager
+            </span>
+            <Button
+              variant="outline"
+              size="icon"
+              content={<ExitIcon className="size-7" />}
+              tooltip="Close"
+              onClick={() => updateSelectedTool(null)}
+            />
+          </div>
+          <div className="flex text-sm rounded-lg items-center bg-input w-[90%] lg:w-[80vw] h-12">
+            <input
+              onChange={handleSearchChange}
+              type="text"
+              className="w-full h-full px-2 text-lg rounded-lg text-foreground bg-input placeholder-secondary-foreground focus:outline-none"
+              placeholder="Search version ie: 18.1"
+            />
+            <MagnifyingGlassIcon className="right-0 mr-1 size-8" />
+          </div>
         </div>
-        <div className="flex text-sm rounded-lg items-center bg-input w-[90%] lg:w-[80vw] h-12">
-          <input
-            onChange={handleSearchChange}
-            type="text"
-            className="w-full h-full px-2 text-lg rounded-lg text-foreground bg-input placeholder-secondary-foreground focus:outline-none"
-            placeholder="Search version ie: 18.1"
-          />
-          <MagnifyingGlassIcon className="right-0 mr-1 size-8" />
+        <div className="flex flex-col items-center justify-around h-full px-2">
+          <Installed />
+          <Available />
         </div>
-      </div>
-      <div className="flex flex-col items-center justify-around h-full px-2">
-        <Installed />
-        <Available />
       </div>
     </div>
   );
