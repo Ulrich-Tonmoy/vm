5b9bd49cd784f7edd056b7129f93544526973b24
Tonmoy <ulrichtonmoy1@gmail.com>
2024-08-10 06:28:23 +0600

    ✨feat: can now clear search using cross button

--- src/components/body/ContentBody.tsx
+++ src/components/body/ContentBody.tsx
@@ -1,18 +1,19 @@
-import { selectedToolAtom, updateSearchTermAtom } from "@/store";
+import { searchTermAtom, selectedToolAtom, updateSearchTermAtom } from "@/store";
 import { useAtomValue, useSetAtom } from "jotai";
 import { Installed, Available } from "./";
 import { useCallback } from "react";
 import { debounce } from "@/libs";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Cross1Icon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
 export const ContentBody = () => {
   const selectedTool = useAtomValue(selectedToolAtom);
+  const searchTerm = useAtomValue(searchTermAtom);
   const updateSearchTerm = useSetAtom(updateSearchTermAtom);
 
   const debouncedSearch = useCallback(
     debounce((value: string) => {
       updateSearchTerm(value);
-    }, 500),
+    }, 50),
     [],
   );
 
@@ -32,13 +33,20 @@ export const ContentBody = () => {
             </span>
           </div>
           <div className="flex text-sm rounded-lg items-center bg-input w-[90%] lg:w-[80vw] h-12">
+            <MagnifyingGlassIcon className="right-0 ml-1 size-8" />
             <input
+              value={searchTerm}
               onChange={handleSearchChange}
               type="text"
               className="w-full h-full px-2 text-lg rounded-lg text-foreground bg-input placeholder-secondary-foreground focus:outline-none"
               placeholder="Search version ie: 18.1"
             />
-            <MagnifyingGlassIcon className="right-0 mr-1 size-8" />
+            {searchTerm && (
+              <Cross1Icon
+                className="right-0 mr-1 size-5 cursor-pointer"
+                onClick={() => updateSearchTerm("")}
+              />
+            )}
           </div>
         </div>
         <div className="flex flex-col items-center justify-around h-full px-2">
