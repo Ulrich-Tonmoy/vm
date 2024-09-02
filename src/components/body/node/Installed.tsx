6b93a83ecaee364384443e558074237c4cc77d13
Tonmoy <ulrichtonmoy1@gmail.com>
2024-09-03 01:39:49 +0600

    ✨feat: set & remove from path and uninstall

--- src/components/body/node/Installed.tsx
+++ src/components/body/node/Installed.tsx
@@ -1,13 +1,90 @@
-import { toolsAtom, searchTermAtom, selectedToolAtom } from "@/store";
-import { CheckIcon, TrashIcon } from "@radix-ui/react-icons";
-import { useAtomValue } from "jotai";
+import {
+  toolsAtom,
+  searchTermAtom,
+  selectedToolAtom,
+  loadNodeVersionAtom,
+  updateConfigAtom,
+} from "@/store";
+import { CheckIcon, ExitIcon, TrashIcon } from "@radix-ui/react-icons";
+import { useAtomValue, useSetAtom } from "jotai";
 import { Button } from "@/components/Button";
+import {
+  dataDirPath,
+  deleteFolder,
+  FileSysRes,
+  NODE_INSTALLED_PATH,
+  removeFromPath,
+  setToPath,
+  showToaster,
+  ToasterType,
+} from "@/libs";
 
 export const Installed = () => {
   const searchTerm = useAtomValue(searchTermAtom);
   const selectedTool = useAtomValue(selectedToolAtom);
   const tools = useAtomValue(toolsAtom);
   const selectedConfig = tools[selectedTool!];
+  const loadNodeVersion = useSetAtom(loadNodeVersionAtom);
+  const updateConfig = useSetAtom(updateConfigAtom);
+
+  const items = selectedConfig.installed
+    .filter((v) => v.includes(searchTerm))
+    .sort((a, b) => {
+      if (a === selectedConfig.active) return -1;
+      if (b === selectedConfig.active) return 1;
+      return 0;
+    });
+
+  const useVersion = async (version: string) => {
+    const path = await dataDirPath();
+    setToPath(NODE_INSTALLED_PATH(path, version));
+    updateConfig({
+      Node: {
+        active: version,
+        installed: [...tools.Node.installed],
+      },
+    });
+    loadNodeVersion(false);
+    showToaster({
+      msg: "Using Node " + version,
+      type: ToasterType.SUCCESS,
+    });
+  };
+
+  const deleteVersion = async (version: string) => {
+    const path = await dataDirPath();
+    const res = await deleteFolder(NODE_INSTALLED_PATH(path, version));
+    if (res === FileSysRes.OK) {
+      const newTools = tools.Node.installed.filter((v) => v !== version);
+      updateConfig({
+        Node: {
+          active: tools.Node.active,
+          installed: [...newTools],
+        },
+      });
+      loadNodeVersion(false);
+      showToaster({
+        msg: "Uninstalled Node " + version,
+        type: ToasterType.WARN,
+      });
+    }
+  };
+
+  const unUseVersion = async (version: string) => {
+    const path = await dataDirPath();
+    removeFromPath([NODE_INSTALLED_PATH(path, version)]);
+    updateConfig({
+      Node: {
+        active: "",
+        installed: [...tools.Node.installed],
+      },
+    });
+    loadNodeVersion(false);
+    showToaster({
+      msg: "UnUsed Node " + version,
+      type: ToasterType.WARN,
+    });
+  };
 
   return (
     <div className="flex flex-col items-center p-4">
@@ -24,15 +101,14 @@ export const Installed = () => {
             <p>Try installing a version from the available list.</p>
           </div>
         ) : (
-          selectedConfig.installed
-            .filter((v) => v.includes(searchTerm))
-            .map((i) => (
-              <div
-                key={i}
-                className="flex items-center justify-center p-1 px-2 rounded-md text-md lg:text-2xl bg-card shadow-md gap-x-2"
-              >
-                <span className="cursor-text select-text">{i}</span>
-                {selectedConfig.active === i ? (
+          items.map((i) => (
+            <div
+              key={i}
+              className="flex items-center justify-center p-1 px-2 rounded-md text-md lg:text-2xl bg-card shadow-md gap-x-2"
+            >
+              <span className="cursor-text select-text">{i}</span>
+              {selectedConfig.active === i ? (
+                <>
                   <Button
                     variant="ghost"
                     size="icon"
@@ -41,26 +117,34 @@ export const Installed = () => {
                     tooltip="Active"
                     onClick={() => {}}
                   />
-                ) : (
-                  <>
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      content={<CheckIcon className="size-8" />}
-                      tooltip={`Use ${i}`}
-                      onClick={() => {}}
-                    />
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      content={<TrashIcon className="size-8 text-rose-500" />}
-                      tooltip={`Uninstall ${i}`}
-                      onClick={() => {}}
-                    />
-                  </>
-                )}
-              </div>
-            ))
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    content={<ExitIcon className="size-8 text-rose-500" />}
+                    tooltip={`Un Use ${i}`}
+                    onClick={() => unUseVersion(i)}
+                  />
+                </>
+              ) : (
+                <div>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    content={<CheckIcon className="size-8" />}
+                    tooltip={`Use ${i}`}
+                    onClick={() => useVersion(i)}
+                  />
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    content={<TrashIcon className="size-8 text-rose-500" />}
+                    tooltip={`Uninstall ${i}`}
+                    onClick={() => deleteVersion(i)}
+                  />
+                </div>
+              )}
+            </div>
+          ))
         )}
       </div>
     </div>
--- src/libs/utils/win-path.ts
+++ src/libs/utils/win-path.ts
@@ -1,18 +1,39 @@
 import { invoke } from "@tauri-apps/api";
 
-export const setToPath = (path: string, prevPath = "") => {
+export const setToPath = (path: string) => {
   invoke("get_user_path").then((message: unknown) => {
     const paths = message as string;
     console.log(paths.split(";"));
 
     let newPath = paths;
-    if (prevPath) {
-      newPath = paths.replace(prevPath, "");
-    }
     if (!newPath.endsWith(";")) {
       newPath += ";";
     }
-    newPath += path;
+
+    if (!newPath.includes(path)) {
+      newPath += path;
+    }
+
+    invoke("set_user_path", { newPath }).then((message: unknown) => {
+      const msg = message as string;
+      console.log(msg);
+    });
+  });
+};
+
+export const removeFromPath = (
+  pathToRemove: string[] = ["%NVM_SYMLINK%;", "C:\\Program Files\\nodejs;"],
+) => {
+  invoke("get_user_path").then((message: unknown) => {
+    const paths = message as string;
+    console.log(paths.split(";"));
+
+    let newPath = paths;
+    if (pathToRemove.length > 0) {
+      pathToRemove.forEach((prev) => {
+        newPath = newPath.replace(prev, "");
+      });
+    }
 
     invoke("set_user_path", { newPath }).then((message: unknown) => {
       const msg = message as string;
